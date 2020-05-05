1dcbe11074f21014bd82f6565dae5876285077b4
VincentBaron <vincentbaron1996@gmail.com>
2020-05-05 15:04:40 +0200

    try

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -10,27 +10,7 @@ size_t  ft_strlen(char *s)
         i++;
     }
     return (i);
-}
-
-char	*ft_strdup(char *s)
-{
-	char	*dup;
-	int		size;
-	int		i;
-
-	size = 0;
-	while (s[size])
-		size++;
-	if (!(dup = malloc(sizeof(char) * (size + 1))))
-		return (0);
-	i = 0;
-	while (i < size)
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = '\0';
-	return (dup);
+	 yo
 }
 
 void    ft_free(char *str)
@@ -69,7 +49,7 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	
 	if (!s1)
-		s1 = ft_strdup("");
+		s1 = ft_substr("", 0, 0);
 	size = ft_strlen(s1) + ft_strlen(s2) + 1;
 	if (!(join = malloc(sizeof(char) * size)))
 		return (0);
