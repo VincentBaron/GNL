fa30dc4cff7891e8febf59d3759518203aa0e341
VincentBaron <vincentbaron1996@gmail.com>
2020-05-28 12:26:29 +0200

    Revert "vOscar"
    This reverts commit 812119df0fcb7cd24df8238d4825ed47efa42b54.

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,13 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/03 00:40:53 by vbaron            #+#    #+#             */
-/*   Updated: 2020/05/28 12:01:58 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/05/11 11:52:58 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	if (!s)
-		return (0);
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-void	ft_free(char *s)
-{
-	free(s);
-	s = NULL;
-}
-
-int		check_line(char *s, char c)
+int		check_line(char *s)
 {
 	int	i;
 
@@ -39,53 +21,47 @@ int		check_line(char *s, char c)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == c)
+		if (s[i] == '\n')
 			return (i);
 		i++;
 	}
 	return (-1);
 }
 
-int	ft_parse(char **leftover, char *buf, int fd)
+void	ft_parse(char **leftover, char *buf, int fd, int *ret)
 {
-	int ret;
-
-	while (check_line(*leftover, '\n') == -1
-	&& (ret = read(fd, buf, BUFFER_SIZE)) > 0)
+	while (check_line(*leftover) == -1
+	&& (*ret = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
-		buf[ret] = '\0';
+		buf[*ret] = '\0';
 		*leftover = ft_strjoin(*leftover, buf);
 	}
-	return (ret);
 }
 
 int		get_next_line(int fd, char **line)
 {
 	char		buf[BUFFER_SIZE + 1];
 	static char	*leftover = NULL;
-	static int ret;
-	static char **lines_list;
-	static int check = 0;
+	char		*temp;
+	int			ret;
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || !line || read(fd, buf, 0) < 0)
 		return (-1);
-	if (check == 0)
+	ft_parse(&leftover, buf, fd, &ret);
+	if (check_line(leftover) > -1)
 	{
-		if(!(lines_list = (char **)malloc(sizeof(char *))))
-			return (-1);
-		*lines_list = NULL;
-		check = 1;
+		*line = ft_substr(leftover, 0, check_line(leftover));
+		temp = ft_substr(leftover, check_line(leftover) + 1,
+		ft_strlen(leftover));
+		ft_free(leftover);
+		leftover = ft_substr(temp, 0, ft_strlen(temp));
+		ft_free(temp);
+		return (1);
 	}
-	if (*lines_list == NULL)
+	else
 	{
-		ret = ft_parse(&leftover, buf, fd);
-		lines_list = ft_split(leftover, '\n');
+		*line = ft_substr(leftover, 0, ft_strlen(leftover));
 		ft_free(leftover);
 	}
-	*line = ft_substr(lines_list[0], 0, ft_strlen(lines_list[0]));
-	ft_free(lines_list[0]);
-	lines_list++;
-	if (ret == 0)
-		return 0;
-	return (1);
+	return (0);
 }
